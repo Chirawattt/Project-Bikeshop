edc2c88367090113d7a166c4d0401c726aab32ff
Chirawattt <chirawat.yana@hotmail.com>
2024-10-03 21:15:05 +0700

    update branch of product [edit/insert]

--- database/migrations/2024_07_17_044834_create_product_table.php
+++ database/migrations/2024_07_17_044834_create_product_table.php
@@ -18,12 +18,14 @@ class CreateProductTable extends Migration
             $table->string('code', 10);
             $table->string('name', 50);
             $table->integer('category_id')->unsigned(); // unsigned for reference to primary key which is all number is greater than 0
+            $table->integer('branch_id')->unsigned(); // unsigned for reference to primary key which is all number is greater than 0
             $table->float('price')->nullable();
             $table->integer('stock_qty')->nullable();
             $table->string('image_url', 200)->nullable();
             $table->timestamps();
             // foreign key
             $table->foreign('category_id')->references('id')->on('category');
+            $table->foreign('branch_id')->references('id')->on('branch');
         });
     }
 
