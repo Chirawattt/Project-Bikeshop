c1396b7c7013f3cd7bbbe238f40e2f65ed676b79
Chirawattt <chirawat.yana@hotmail.com>
2024-10-04 00:03:17 +0700

    update master.blade of user

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,27 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>BikeShop | @yield('title', 'จำหน่ายจักรยานออนไลน์')</title>
-    {{-- css --}}
-    {{-- public/vendor -> bootstrap, font-awesome, toastr --}}
-    {{-- downloaded it to use in this project --}}
+
+    {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/font-awesome/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/toastr/build/toastr.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    {{-- js --}}
+
+    {{-- JS --}}
     <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('vendor/toastr/build/toastr.min.js') }}"></script>
     <script src="{{ asset('js/angular.min.js') }}"></script>
 
-    {{-- define angular module to use entirely app --}}
+    {{-- Define Angular module --}}
     <script>
-        // angular.module('name', [dependencies]) -> create module [] means no dependencies create module from scratch
         const app = angular.module('app', []).config(function($interpolateProvider) {
             $interpolateProvider.startSymbol('@{').endSymbol('}');
         });
     </script>
-
 </head>
 
 <body>
@@ -39,10 +37,9 @@
             <p class="navbar-text navbar-left">นายจีรวัฒน์ ญานะ 6506021611017</p>
             <div id="navbar" class="navbar-collapse collapse navbar-right">
                 <ul class="nav navbar-nav">
-
                     @guest
                         <li><a href="{{ route('login') }}">ล็อกอิน</a></li>
-                        <li><a href="{{ route('register')}}">ลงทะเบียน</a></li>
+                        <li><a href="{{ route('register') }}">ลงทะเบียน</a></li>
                     @endguest
 
                     @auth
@@ -59,18 +56,20 @@
                             <li><a href="/home">หน้าแรก</a></li>
                             <li><a href="/category">ประเภทสินค้า</a></li>
                             <li><a href="#">ข้อมูลการสั่งซื้อสินค้า</a></li>
-                            <li><a href="/logout">ออกจากระบบ</a></li> 
+                            <li><a href="/logout">ออกจากระบบ</a></li>
                         @elseif (auth()->user()->level == 'customer')
-                            <li><a href="/cart/view"> 
-                                <i class="fa fa-shopping-cart"></i> ตะกร้า
-                                <span class="label label-danger">
-                                    @if (Session::has('cart_items'))
-                                        {{ count(Session::get('cart_items')) }}
-                                    @else
-                                        {{ count([]) }}
-                                    @endif
-                                </span>
-                            </a></li>
+                            <li>
+                                <a href="/cart/view">
+                                    <i class="fa fa-shopping-cart"></i> ตะกร้า
+                                    <span class="label label-danger">
+                                        @if (Session::has('cart_items'))
+                                            {{ count(Session::get('cart_items')) }}
+                                        @else
+                                            {{ count([]) }}
+                                        @endif
+                                    </span>
+                                </a>
+                            </li>
                             <li><a href="#">{{ Auth::user()->name }}</a></li>
                             <li><a href="/category">รายการเมนู</a></li>
                             <li><a href="/home">หน้าแรก</a></li>
@@ -78,25 +77,6 @@
                             <li><a href="/logout">ออกจากระบบ</a></li>
                         @endif
                     @endauth
-                    <li><a href="/home">หน้าแรก</a></li>
-                    <li><a href="{{ route('login') }}">ล็อกอิน</a></li>
-                    <li><a href="{{ route('register')}}">ลงทะเบียน</a></li>
-                    @else
-                    <li><a href="/cart/view"> 
-                        <i class="fa fa-shopping-cart"></i> ตะกร้า
-                        <span class="label label-danger">
-                            @if (Session::has('cart_items'))
-                                {{ count(Session::get('cart_items')) }}
-                            @else
-                                {{ count([])}}
-                            @endif
-                        </span>
-                    </a></li>
-                    <li><a href="#">{{ Auth::user()->name }}</a></li>
-                    <li><a href="/product">ข้อมูลสินค้า</a></li>
-                    <li><a href="/category">ประเภทสินค้า</a></li>
-                    <li><a href="/order">รายการสั่งซื้อ</a></li>
-                    <li><a href="/logout">ออกจากระบบ</a></li>@endguest
                 </ul>
             </div>
         </div>
@@ -106,16 +86,12 @@
         @yield('content')
     </div>
 
-    {{-- js --}}
+    {{-- JS for Toastr notifications --}}
     @if (session('message'))
-        @if (session('status'))
-            <script>
-                toastr.success("{{ session('message') }}")
-            </script>
-        @else
-            <script>
-                toastr.error("{{ session('message') }}")
-            </script>
-        @endif
+        <script>
+            toastr.{{ session('status') ? 'success' : 'error' }}("{{ session('message') }}");
+        </script>
     @endif
 </body>
+
+</html>
