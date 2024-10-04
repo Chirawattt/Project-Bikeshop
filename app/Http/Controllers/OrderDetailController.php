34e74c720956070a798b3ed55deb6de2b2af1a12
gawston <naruenat.privatez@gmail.com>
2024-10-04 21:41:54 +0700

    update

--- app/Http/Controllers/OrderDetailController.php
+++ app/Http/Controllers/OrderDetailController.php
@@ -9,19 +9,12 @@ use App\Models\Order;
 use App\Models\Order_Detail;
 
 class OrderDetailController extends Controller {
-    public function displayDetail($order_number)
-    {
-        // ค้นหา Order โดยใช้ order_number
+    public function displayDetail($order_number) {
         $order = Order::where('order_number', $order_number)->first();
-    
-        // ตรวจสอบว่าพบ Order หรือไม่
         if (!$order) {
             return redirect()->back()->with('status', false)->with('message', 'ควยไรสัส');
         }
-    
-        // ดึงข้อมูล Order_Detail โดยใช้ order_id จาก Order ที่เจอ
         $orderDetails = Order_Detail::where('order_id', $order->id)->get();
-    
         return view('order/orderDetail', compact('order', 'orderDetails'));
     }
     
@@ -39,23 +32,16 @@ class OrderDetailController extends Controller {
         return redirect('/home')->with('status', true)->with('message', 'สั่งซื้อสินค้าสำเร็จ');
     }
 
-    public function updatePaymentStatus(Request $request)
-    {
-        // รับค่าผ่าน POST method
+    public function updatePaymentStatus(Request $request) {
         $order_id = $request->input('order_id');
         $payment_status = $request->input('payment_status');
         $order_number = $request->input('order_number');
-    
-        // ค้นหา Order
         $order = Order::where('order_number', $order_number)->first();
         if (!$order) {
             return redirect()->back()->with('status', false)->with('message', 'ควยไรสัส 2');
         }
-    
-        // อัปเดตสถานะการชำระเงิน
         $order->payment_status = $payment_status;
         $order->save();
-    
         return redirect()->back()->with('status', true)->with('message', 'ได้แล้วสัส');
     }
     
